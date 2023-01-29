56ea1f278c57b36445a2c0d9b5ddab62cdc3b25b
Diego Bernardes <113109526+DiegoBernardes95@users.noreply.github.com>
2023-01-28 23:33:33 -0300

    atualização da sintaxe do script

--- SQL/Resilia_database.sql
+++ SQL/Resilia_database.sql
@@ -1,41 +1,41 @@
-create database Resilia_database;
-use Resilia_database;
+create database `Resilia_database`;
+use `Resilia_database`;
 
-create table Curso(
-id int primary key not null auto_increment,
-Nome varchar(20),
-Modulos int,
-Qtd_de_turmas int,
-Carga_horaria varchar(10),
-Requisitos text(200),
-Descricao text(600));
+create table `Curso`(
+`id` int primary key not null auto_increment,
+`Nome` varchar(20),
+`Modulos` int,
+`Qtd_de_turmas` int,
+`Carga_horaria` varchar(10),
+`Requisitos` text(200),
+`Descricao` text(600));
 
-create table Materia(
-id int primary key not null auto_increment,
-Nome varchar(30),
-Carga_horaria varchar(10),
-Descricao text(600));
+create table `Materia`(
+`id` int primary key not null auto_increment,
+`Nome` varchar(30),
+`Carga_horaria` varchar(10),
+`Descricao` text(600));
 
-create table Professor(
-id int primary key not null auto_increment,
-Nome varchar(20),
-Matricula varchar(20),
-Telefone varchar(20),
-Turmas_onde_leciona varchar(20),
-Endereco varchar(70));
+create table `Professor`(
+`id` int primary key not null auto_increment,
+`Nome` varchar(20),
+`Matricula` varchar(20),
+`Telefone` varchar(20),
+`Turmas_onde_leciona` varchar(20),
+`Endereco` varchar(70));
 
-create table Aluno(
-id int primary key not null auto_increment,
-Nome varchar(20),
-Matricula varchar(20),
-Media_notas float,
-Telefone varchar(20),
-Endereco varchar(70));
+create table `Aluno`(
+`id` int primary key not null auto_increment,
+`Nome` varchar(20),
+`Matricula` varchar(20),
+`Media_notas` float,
+`Telefone` varchar(20),
+`Endereco` varchar(70));
 
-create table Turma(
-id int primary key not null auto_increment,
-Inicio_das_aulas date,
-Fim_das_aulas date,
-Qtd_de_alunos int,
-Turno varchar(10),
-Media_geral float);
+create table `Turma`(
+`id` int primary key not null auto_increment,
+`Inicio_das_aulas` date,
+`Fim_das_aulas` date,
+`Qtd_de_alunos` int,
+`Turno` varchar(10),
+`Media_geral` float);
